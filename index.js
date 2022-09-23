730e5ba96c783f713b77bf4c8b3e64dbd1ba58fe
Mclilzee <mc.lilzee@hotmail.com>
2022-09-23 07:23:35 +0200

    Add price per gram / milileter display

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const form = document.querySelector("form");
 const resultInKg = document.querySelector(".result.kg");
-const resultInHundretGrams = document.querySelector(".result.g")
+const resultInHundretGrams = document.querySelector(".result.hundret-g");
+const resultInGrams = document.querySelector(".result.g");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -9,6 +10,7 @@ form.addEventListener("submit", (event) => {
   const pricePerGram = price / weight;
   resultInKg.textContent = (pricePerGram * 1000).toFixed(2);
   resultInHundretGrams.textContent = (pricePerGram * 100).toFixed(2);
+  resultInGrams.textContent = pricePerGram;
 
   event.target[1].blur();
 })
